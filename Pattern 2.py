a3ea0f7e4410015d67575b3cf88610c3b41f5df4
Chandana Adusumalli <a.chandana1709@gmail.com>
2025-01-10 17:51:40 -0600

    Right traingle star pattern

---
+++ Pattern 2.py
@@ -0,0 +1,14 @@
+'''   *
+      **
+      ***
+      ****
+      *****  '''
+
+def pattern(n):
+    for i in range(n):
+        for j in range(i+1):
+            print("*",end="")
+        print()
+
+n=5
+pattern(n)
