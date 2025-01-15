aeedc0b6c943ee53c087d3ca029a69792528581a
Chandana Adusumalli <a.chandana1709@gmail.com>
2025-01-14 23:36:17 -0600

    Right traingle star pattern

---
+++ Pattern 3.py
@@ -0,0 +1,13 @@
+# Right Triangle star pattern
+def pattern(n):
+    for i in range(n):
+        #To print spaces
+        for j in range(n-i):
+            print(" ",end="")
+        for k in range(i+1):
+            print("*",end="")
+        # To print in next line
+        print()
+
+n=5
+pattern(n)
